b4c0e0d2752d82a9fc8615575c286341eb87b2ac
Carlton <carlton@linux.com>
2019-08-29 22:31:05 +0100

    Add block decryption and recovery to verifier

--- emlog/verifier.py
+++ emlog/verifier.py
@@ -8,13 +8,22 @@ from cryptography.hazmat.primitives.asymmetric import ec
 from cryptography.hazmat.primitives.ciphers.aead import AESGCM
 from cryptography.hazmat.primitives.kdf.hkdf import HKDF
 
+logger = logging.getLogger()
+
 class Verifier:
-    def __init__(self, fpath):
-        # Unpickle data
-        unpickled_data = pickle.loads(fpath)
+    def __init__(self, rlk_secret, storage_key):
+        self.aesgcm = AESGCM(storage_key)
 
-    def __str__(self):
-        pass
-
-    def _load_data(self, fpath):
-        pass
+    def verify(data):
+        encrypted_blocks = pickle.loads(data)
+        logger.debug(f"Pickled data: {self.encrypted_blocks}")
+        # Each block in encrypted_blocks contains an iv and enc_bytes.
+        # enc_bytes is an encrypted pickled representation of the block,
+        # keyed under storage_key with iv.
+        for enc_block in encrypted_blocks:
+            logger.debug(f"enc_block: {enc_block}")
+            pkl_block = self.aesgcm.decrypt(enc_block['iv'], enc_block['enc_bytes'], None)
+            block = pickle.loads(pkl_block)
+            logger.debug(f"Unpickled block: {block}")
+            logger.debug(f"block sig: {block.sig}")
+            logger.debug(f"block id: {block.block_id}")
